de7829b271c12ae7fcb67c0a30507db4cd83d196
Renat Gofman <renatgofman@gmail.com>
2024-09-10 20:54:18 +0300

    change database local

--- scripts/prepare_testnet.sh
+++ scripts/prepare_testnet.sh
@@ -9,14 +9,11 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 echo "Preparing subgraph for ${yellow}$NETWORK${reset} network" && echo;
-npx mustache ./config/${NETWORK}.${VERSION}.json ./templates/${BLOCKCHAIN}.subgraph.template.yaml > subgraph.yaml && rm -rf generated;
+npx mustache ./config/${NETWORK}.${VERSION}.json ./templates/${BLOCKCHAIN}.subgraph.template.yaml > subgraph.yaml
 echo "File: ${green}subgraph.yaml${reset} has been mustached";
 echo "copying schema... ${VERSION}";
 cp ./schemas/${VERSION}.schema.graphql schema.graphql;
-echo "Build starting...";
-npm run codegen && npm run build;
 echo "File: ${green}subgraph.yaml${reset} has been mustached";
-npx npm run deploy;
 echo "File: ${green}src/config/contracts.ts${reset} has been mustached";
 npx mustache config/${NETWORK}.${VERSION}.json docker-compose.template.yml > docker-compose.yml;
 echo "File: ${green}docker-compose.yml${reset} has been mustached";
